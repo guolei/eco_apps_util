4b5da3b62a319b528035c05b46484dec6137d75e
Guo Lei <guolei@idapted.com>
2011-01-10 06:17:19 +0800

    add EcoApps::Const

--- lib/utils.rb
+++ lib/utils.rb
@@ -6,8 +6,8 @@ module EcoApps
         OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA1.new, salt || random_salt, raw_data.to_s)
       end
 
-      def random_salt
-        ActiveSupport::SecureRandom.hex(64)
+      def random_salt(size = 64)
+        ActiveSupport::SecureRandom.hex(size)
       end
 
       def env_value(value)
@@ -16,6 +16,14 @@ module EcoApps
 
       def escape(url)
         URI.escape(url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+      end
+    end
+  end
+  
+  class Const
+    class << self
+      def security_token
+        "a2f88d5008d3364b1c31fe42d4d6472084d679afdbb4a37ba3aeeaf4386944c80989eeb892aa8e3bde6e357214938ce02eb9e2786333e13450ddc29cb1e81770"
       end
     end
   end
